4569c7545fba213c2c0b1f756b4775be40309d6b
Jeongsebin <101381141+Jeongsebin@users.noreply.github.com>
2022-03-11 14:11:50 +0900

    Add files via upload

---
+++ image operation.py
@@ -0,0 +1,9 @@
+from scipy.misc import imread, imsave, imresize
+
+img = imread('cat.jpg')
+print(img.dtype, img.shape)
+
+img_tined = imresize(img_tinted, (300, 300))
+
+imsave('cat_tinted.jpg', img_tinted)
+print(img_tinted.dtype, img_tinted.shape)
