31d6338746c56b950712bbade0c1641e3a4c93f2
Jeongsebin <101381141+Jeongsebin@users.noreply.github.com>
2022-03-18 19:32:46 +0900

    Update image operation.py

--- image operation.py
+++ image operation.py
@@ -4,7 +4,7 @@ from scipy.misc import imread, imsave, imresize
 img = imread('cat.jpg')
 print(img.dtype, img.shape)
 
-img_tined = imresize(img_tinted, (300, 300))
+img_tined = imresize(img_tinted, (300, 300)) //(행, 열) pixel 단위
 
 imsave('cat_tinted.jpg', img_tinted)
 print(img_tinted.dtype, img_tinted.shape)
